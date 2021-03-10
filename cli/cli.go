7b5462436fbcf10ba589b1b6c10696f87d7f0141
MarshallRawson <marshallrawson@ufl.edu>
2021-03-09 23:15:07 -0500

    incremental

--- cli/cli.go
+++ cli/cli.go
@@ -9,29 +9,46 @@ import (
 )
 
 func Run() {
+	func_map := map[string]func([]string) *block_chain.BlockChain{
+		"genesis": genesis,
+		"load":    not_yet_supported,
+		"join":    not_yet_supported}
 	for {
-		fmt.Println("[load [title], genesis [name, title]]")
+		fmt.Println("\n[load [title], genesis [name, title], join]")
 		fmt.Printf("->")
 		reader := bufio.NewReader(os.Stdin)
 		input, _ := reader.ReadString('\n')
 		args := strings.Fields(input)
 		var bc *block_chain.BlockChain = nil
-		if args[0] == "genesis" {
-			// genesis
-			//bc = block_chain.New(block_chain.Self{
-		} else if args[0] == "load" {
-			// load
-			fmt.Println("not yet supported")
+		if _, ok := func_map[args[0]]; ok == false {
+			bc = unknown_command(args[1:])
+		} else {
+			bc = func_map[args[0]](args[1:])
 		}
-
-		if bc != nil {
-			for {
-				// post
-				// change_name
-				// add publisher
-				// add node
-				// save and exit
-			}
+		if bc == nil {
+			continue
+		}
+		for {
+			// post
+			// change_name
+			// add publisher
+			// add node
+			// save and exit
 		}
 	}
+}
+
+func genesis(args []string) *block_chain.BlockChain {
+
+	return nil
+}
+
+func unknown_command(args []string) *block_chain.BlockChain {
+	fmt.Println("This command is unknown")
+	return nil
+}
+
+func not_yet_supported(args []string) *block_chain.BlockChain {
+	fmt.Println("This command is not yet supported")
+	return nil
 }
